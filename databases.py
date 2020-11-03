696510654a3f86e85c5f0224509388f01067086d
tuckmull <47407074+tuckmull@users.noreply.github.com>
2020-11-02 20:45:31 -0500

    New updates to files

--- databases.py
+++ databases.py
@@ -20,10 +20,15 @@ def makeCurrentlyRegistered(availableClasses):
     df = pd.DataFrame(columns=availableClasses.columns)
     return df
 
+availableClasses = makeAvailableClasses("csvFiles/Class Database - AvailableClasses.csv")
+taken = makeHasTaken("csvFiles/Class Database - HasTaken.csv")
+required = makeRequiredCourses("csvFiles/Class Database - RequiredCourses.csv")
+currentSchedule = makeCurrentlyRegistered(availableClasses)
+#print(availableClasses)
 
 def getFulfilledDivisionals():
     q1 = "select Division, sum(Hours) as totalHours from taken group by Division"
-    print(ps.sqldf(q1))
+    #print(ps.sqldf(q1))
     return ps.sqldf(q1)
 
 def getMissingDivisionals():
@@ -57,9 +62,69 @@ def getClassesThatFulfill(division, available):
 
 def registerCourse(courseName, available, schedule):
     course = available.loc[available['Title'] == courseName]
-    print(course)
+    #print(course)
     newSchedule = schedule.append(course)
     return newSchedule
+
+
+def getCSCRequirementsLeft(df):
+    """
+    This function returns the CSC requirements left for a student to take when
+    you input the dataframe of the classes the student has taken into the
+    function.
+    """
+    csc_requirements = pd.read_csv("csvFiles/CSC BS Requirements/Class Database - CSCRequirements.csv")
+    csc_electives = pd.read_csv("csvFiles/CSC BS Requirements/Class Database - CSCElectives.csv")
+    mst_requirements = pd.read_csv("csvFiles/CSC BS Requirements/Class Database - MSTRequirements.csv")
+    csc_requirements = csc_requirements.sort_values(
+        by="Course",
+        axis=0
+    )
+    csc_electives = csc_electives.sort_values(
+        by="Course",
+        axis=0
+    )
+    mst_requirements = mst_requirements.sort_values(
+        by="Course",
+        axis=0
+    )
+
+    csc_taken = df[df["Subject"] == "CSC"]
+    csc300_count = 0
+
+    for course_taken in csc_taken["Course"].to_numpy():
+        if ((course_taken > 300) and (course_taken in csc_electives["Course"].to_numpy())):
+            csc300_count += 1
+            csc_electives = csc_electives.drop(
+                index=csc_electives.loc[csc_electives["Course"] == course_taken].index
+            )
+        if (course_taken in csc_requirements["Course"].to_numpy()):
+            csc_requirements = csc_requirements.drop(
+                index=csc_requirements.loc[csc_requirements["Course"] == course_taken].index
+            )
+
+    mst_taken = df[df["Subject"] == "MST"]
+
+    for course_taken in mst_taken["Course"].to_numpy():
+        if (course_taken in mst_requirements["Course"].to_numpy()):
+            mst_requirements = mst_requirements.drop(
+                index=mst_requirements.loc[mst_requirements["Course"] == course_taken].index
+            )
+            if course_taken in [121, 205, 206]:
+                for required_course in [121, 205, 206]:
+                    if required_course != course_taken:
+                        mst_requirements = mst_requirements.drop(
+                            index=mst_requirements.loc[mst_requirements["Course"] == required_course].index
+                        )
+
+    csc_requirements = pd.concat([csc_requirements, csc_electives], axis=0)
+    csc_requirements = pd.concat([csc_requirements, mst_requirements], axis=0)
+    csc_requirements = csc_requirements.sort_values(
+        by="Course",
+        axis=0
+    )
+
+    return csc_requirements
 
 
 #instantiates dataframes from excel file
@@ -69,19 +134,19 @@ required = makeRequiredCourses("csvFiles/Class Database - RequiredCourses.csv")
 
 #just to put identical columns
 currentSchedule = makeCurrentlyRegistered(availableClasses)
-print(availableClasses)
+#print(availableClasses)
 
 
 missingDivisionals = getMissingDivisionals()
-print(missingDivisionals)
+#print(missingDivisionals)
 
 currentSchedule = registerCourse('Introduction ',availableClasses,currentSchedule)
-print(currentSchedule)
+#print(currentSchedule)
 
 toFulfill3 = getClassesThatFulfill(missingDivisionals[0],availableClasses)
-print(toFulfill3)
+#print(toFulfill3)
 
 toFulfillAll = getClassesThatFulfill(missingDivisionals,availableClasses)
-print(toFulfillAll)
+#print(toFulfillAll)
 
-
+getCSCRequirementsLeft(taken)
